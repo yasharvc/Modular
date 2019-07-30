92d2aad9372cdabce57817a6d8d5a15465c5d6e4
Yashar aliabbasi <Yasharvc@yahoo.com>
2019-07-30 15:50:49 +0430

    Simple API Testing

---
+++ Test/BSThemeWithAuthentication/Controllers/APIController.cs
@@ -0,0 +1,20 @@
+﻿using Contracts.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BSThemeWithAuthentication.Controllers
+{
+	public class APIController : Controller
+    {
+		[AuthenticationType(typeof(AnonymousAuthentication))]
+        public JsonResult Free()
+        {
+            return Json(new { name="yashar",family="aliabbasi" });
+        }
+
+		[AuthenticationType(typeof(BTAuthentication))]
+		public JsonResult auth()
+		{
+			return Json(new { name = "yashar", family = "aliabbasi", Authentication = nameof(BTAuthentication) });
+		}
+    }
+}
