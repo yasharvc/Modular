2042debd6dc6cbbc676d029ab2ba6333bfff8b22
Yashar aliabbasi <Yasharvc@yahoo.com>
2019-08-14 08:18:48 +0430

    Clearing Cde

--- Modules/DefaultTheme/Components/ModulesMenuViewComponent.cs
+++ Modules/DefaultTheme/Components/ModulesMenuViewComponent.cs
@@ -1,7 +1,7 @@
-﻿using Microsoft.AspNetCore.Mvc;
-using System.Threading.Tasks;
+﻿using Contracts.MVC;
 using Contracts.ViewComponent;
-using Contracts.MVC;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace DefaultTheme.Components
 {
